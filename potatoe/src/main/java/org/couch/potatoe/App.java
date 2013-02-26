847f3d6997e18ed7dec2b9cc03d81d07385716ba
Thorsten Castor <t.j.castor@gmail.com>
2013-02-26 17:14:52 +1300

    localised example

--- potatoe/src/main/java/org/couch/potatoe/App.java
+++ potatoe/src/main/java/org/couch/potatoe/App.java
@@ -30,23 +30,19 @@ public class App {
     private static final Logger log = LoggerFactory.getLogger(App.class);
 
     // the unique key of the document
-    public static final String KEY = "beer_Wrath";
+    public static final String KEY = "beer_Czar";
 
     // expiration time (in seconds) of the document (use 0 to persist forever)
-    public static final int EXP_TIME = 10;
+    public static final int EXP_TIME = 0;
 
 
     public static void main(String args[]) {
 
-        // Set the URIs and get a client
         List<URI> uris = new LinkedList<URI>();
-
-        // Connect to localhost or to the appropriate URI(s)
         uris.add(URI.create("http://ubuntu:8091/pools"));
 
         CouchbaseClient client = null;
         try {
-            // Use the "default" bucket with no password
             client = new CouchbaseClient(uris, "beer-sample", "");
         }
         catch (IOException e) {
@@ -54,7 +50,18 @@ public class App {
             System.exit(1);
         }
 
-        set(client);
+        Beer beer = new App().new Beer();
+        beer.name = "Czar";
+        beer.abv = 8.5f;
+        beer.type = "beer";
+        beer.brewery_id = "Wigram";
+        beer.updated = new Date();
+        beer.description = "Brewed in the authentic historical style of an Imperial Russian Stout.";
+//        beer.description = "Brewed in the authentic historical style of an Imperial Russian Stout, this big bold beer is crafted from rich roasted and toasted malts, fermented with a classic English ale yeast this Czarist favourite is an intense, living, bottle condition, full bodied Stout. At 8.5% abv The Czar lives up to it's name.";
+        beer.style = "Imperial Stout";
+        beer.category = "Imperial Russian Stout";
+
+        set(client, beer);
         view(client);
 
         // Shutdown and wait a maximum of three seconds to finish up operations
@@ -64,8 +71,7 @@ public class App {
 
 
     /**
-     * assumes view with following JS map
-     * <code>
+     * assumes view with following JS map <code>
      * function (doc, meta) {
      *   if (doc.type && doc.name && doc.type == "beer") {
      *      emit(doc.name, meta.id);
@@ -78,7 +84,7 @@ public class App {
         View view = client.getView("beer", "by_name");
 
         Query query = new Query();
-        query.setKey("Wrath"); // Only retrieve this specific key
+        query.setKey("Czar"); // Only retrieve this specific key
         query.setIncludeDocs(true); // Include the full document as well
 
         ViewResponse result = client.query(view, query);
@@ -98,23 +104,24 @@ public class App {
     }
 
 
-    private static void set(CouchbaseClient client) {
-        Beer beer = new App().new Beer();
-        beer.name = "Wrath";
-        beer.abv = 0.9f;
-        beer.type = "beer";
-        beer.brewery_id = "110f1a10e7";
-        beer.updated = new Date();
-        beer.description = "WRATH Belgian-style";
-        beer.style = "Other Belgian-Style Ales";
-        beer.category = "Belgian and French Ale";
+    private static void set(CouchbaseClient client, Beer beer) {
 
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd hh:mm:ss").create();
 
-        // Do an asynchronous set
-        OperationFuture<Boolean> setOp = client.set(KEY, EXP_TIME, gson.toJson(beer));
-
         try {
+            OperationFuture<Boolean> addOp = client.add(KEY, EXP_TIME, gson.toJson(beer));
+            if (addOp.get().booleanValue()) {
+                log.info("Add Succeeded");
+                String object = (String) client.get(KEY);
+                log.info("get: " + KEY + ": " + object);
+
+            }
+            else {
+                log.error("Add failed: " + addOp.getStatus().getMessage());
+            }
+
+            OperationFuture<Boolean> setOp = client.set(KEY, EXP_TIME, gson.toJson(beer));
+
             if (setOp.get().booleanValue()) {
                 log.info("Set Succeeded");
                 String object = (String) client.get(KEY);
